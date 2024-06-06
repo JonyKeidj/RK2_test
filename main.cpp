fecb1ed78328301933a5f5801e2b2aaaf202d64c
JonyKeidj <123669070+JonyKeidj@users.noreply.github.com>
2024-06-06 11:01:05 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include "BusinessSystem.h"
+
+int main()
+{
+    EstateOwner estateOwner;
+    GroceryStore groceryStore;
+    Restaurant restaurant;
+
+    BusinessMediator mediator(estateOwner, groceryStore, restaurant);
+
+    SupplyGrocery(groceryStore, 2);
+    groceryStore.Sell();
+    BuyFood(restaurant);
+    BuyFood(restaurant);
+    std::cout << std::endl;
+
+    SupplyGrocery(groceryStore, 3);
+    ChangeEstateRentPrice(estateOwner, 1000);
+    BuyFood(restaurant);
+    ChangeEstateRentPrice(estateOwner, 10000);
+    BuyFood(restaurant);
+    ChangeEstateRentPrice(estateOwner, 100000);
+    BuyFood(restaurant);
+    std::cout << std::endl;
+
+    SupplyGrocery(groceryStore, 3);
+    ChangeGroceryPrice(groceryStore, 100);
+    BuyFood(restaurant);
+    ChangeEstateRentPrice(estateOwner, 10000);
+    BuyFood(restaurant);
+    ChangeGroceryPrice(groceryStore, -100);
+    BuyFood(restaurant);
+    BuyFood(restaurant);
+}
